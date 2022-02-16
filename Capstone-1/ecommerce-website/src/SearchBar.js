2dde27d2bc2e33eb1f67550de4d3db6dceba3205
Jonah Pena <jonahrpena@gmail.com>
2022-02-16 15:14:08 -0600

    working through search functionality.

--- Capstone-1/ecommerce-website/src/SearchBar.js
+++ Capstone-1/ecommerce-website/src/SearchBar.js
@@ -2,11 +2,25 @@ import React from "react";
 import "./SearchBar.css";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <div className="searchContainer">
-        <form className="formContainer">
-          <input placeholder="Search" className="inputText" type="text" />
+        <form onSubmit={this.onFormSubmit} className="formContainer">
+          <input
+            placeholder="Search"
+            onChange={(e) => this.setState({ term: e.target.value })}
+            className="inputText"
+            type="text"
+            value={this.state.term}
+          />
         </form>
       </div>
     );
