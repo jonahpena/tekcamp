bb8f1d91f43b623e0643b9a0a9f5daf6302d452e
Jonah Pena <jonahrpena@gmail.com>
2022-03-02 14:33:42 -0600

    refactored to OOP successfully

--- java_exercises/src/main/java/com/teksystems/bootcamp/java_exercises/additionalInputs/Main.java
+++ java_exercises/src/main/java/com/teksystems/bootcamp/java_exercises/additionalInputs/Main.java
@@ -1,22 +1,23 @@
 package com.teksystems.bootcamp.java_exercises.additionalInputs;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        NumberAggregator aggregator = new NumberAggregator();
+
         int total = 0;
         System.out.println("Enter an Integer: ");
 
+        int userInput = Integer.valueOf(scanner.nextLine());
 
-        while (true) {
-            int userInput = Integer.valueOf(scanner.nextLine());
-            if (userInput != 0) {
-                total = total + userInput;
-                System.out.println("Enter another Integer to Add to the sum: ");
-            } else {
-                System.out.println("The sum is: " + total);
-            }
+        while (userInput != 0) {
+            total = aggregator.addNumber(userInput);
+            System.out.println("Enter another Integer to Add to the sum: ");
+            userInput = Integer.valueOf(scanner.nextLine());
         }
+        System.out.println("The sum is: " + total);
     }
 }
--- java_exercises/src/main/java/com/teksystems/bootcamp/java_exercises/additionalInputs/NumberAggregator.java
+++ java_exercises/src/main/java/com/teksystems/bootcamp/java_exercises/additionalInputs/NumberAggregator.java
@@ -1,7 +1,9 @@
 package com.teksystems.bootcamp.java_exercises.additionalInputs;
 
 public class NumberAggregator {
-    public int addNumber; // Field
+    public int total;
 
-
+    public int addNumber(int userInput) {
+        return total = (total + userInput);
+    }
 }
