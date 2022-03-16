370830b3f23da845fd63b2d52593989e8f82c5d9
Jonah Pena <jonahrpena@gmail.com>
2022-03-16 00:23:01 -0500

    added combo menu

--- capstone2/src/main/java/com/teksystems/bootcamp/capstone2/Menu.java
+++ capstone2/src/main/java/com/teksystems/bootcamp/capstone2/Menu.java
@@ -268,4 +268,229 @@ public class Menu {
         }
     }
 
+
+
+    private void chooseEntreeForCombo(int choice) {
+        switch (choice) {
+            case 0:
+                exit = true;
+                System.out.println("\nThank you for choosing The Poke Palace today! Have a great day.");
+                break;
+            case 1:
+                System.out.println("\nYou've chosen the Aloha Special for 12.99.");
+                AddAlohaEntree.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 2:
+                System.out.println("\nYou've chosen the Ono Special for 12.99.");
+                AddOnoEntree.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 3:
+                System.out.println("\nYou've chosen the Ohana Special for 12.99.");
+                AddOhanaEntree.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 4:
+                System.out.println("\nYou've chosen the Mahalo Special for 12.99.");
+                AddMahaloEntree.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 5:
+                UserInterface.printDrinkMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getDrinkInput();
+                break;
+            case 6:
+                UserInterface.printSideMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+
+        }
+    }
+
+    public static void chooseToppingForCombo(int choice) {
+        boolean exit;
+        switch (choice) {
+            case 0:
+                exit = true;
+                System.out.println("\nThank you for choosing The Poke Palace today! Have a great day.");
+                break;
+            case 1:
+                System.out.println("\nYou've added Furikake to your meal. Use the menu below to continue with your order.");
+                AddFurikakeTopping.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 2:
+                System.out.println("\nYou've added Ginger to your meal. Use the menu below to continue with your order.");
+                AddGingerTopping.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 3:
+                System.out.println("\nYou've added Masago to your meal. Use the menu below to continue with your order.");
+                AddMasagoTopping.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 4:
+                System.out.println("\nYou've added Wasabi to your meal. Use the menu below to continue with your order.");
+                AddWasabiTopping.addToOrder();
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 5:
+                UserInterface.printDrinkMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getDrinkInput();
+                break;
+            case 6:
+                UserInterface.printSideMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 7:
+                UserInterface.printEntreeMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                break;
+            case 8:
+                Receipt.printReceipt();
+                UserInterface.additionalOrderOptionsMenu();
+                Input.getAdditionalOrderInput();
+                break;
+        }
+    }
+
+    public static void chooseDrinkForCombo(int choice) {
+        boolean exit;
+        switch (choice) {
+            case 0:
+                exit = true;
+                System.out.println("\nThank you for choosing The Poke Palace today! Have a great day.");
+                break;
+            case 1:
+                System.out.println("\nYou've added a Sprite to your meal. Use the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddSprite.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 2:
+                System.out.println("\nYou've added a Coke to your meal. Use the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddCoke.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 3:
+                System.out.println("\nYou've added a Root-Beer to your meal. Use the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddRootBeer.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 4:
+                System.out.println("\nYou've added a Water to your meal. Use the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddWater.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 5:
+                UserInterface.printSideMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 6:
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 7:
+                UserInterface.printEntreeMenu();
+                DisplayCurrentOrder.displayCurrentOrder();;
+                break;
+            case 8:
+                Receipt.printReceipt();
+                UserInterface.additionalOrderOptionsMenu();
+                Input.getAdditionalOrderInput();
+                break;
+        }
+    }
+
+    public static void chooseSideForCombo(int choice) {
+        boolean exit;
+        switch (choice) {
+            case 0:
+                exit = true;
+                System.out.println("\nThank you for choosing The Poke Palace today! Have a great day.");
+                break;
+            case 1:
+                System.out.println("\nYou've added a side of Edamame to your meal. \nUse the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddEdamame.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 2:
+                System.out.println("\nYou've added a Crab ball to your meal. \nUse the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddCrabBall.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 3:
+                System.out.println("\nYou've added a side of Spam Musubi to your meal. \nUse the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddSpamMusubi.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 4:
+                System.out.println("\nYou've added a side of Nori to your meal. \nUse the menu below to continue with your order.");
+                UserInterface.printSideMenu();
+                AddNori.addToOrder();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getSideInput();
+                break;
+            case 5:
+                UserInterface.printToppingMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getToppingInput();
+                break;
+            case 6:
+                UserInterface.printDrinkMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                Input.getDrinkInput();
+                break;
+            case 7:
+                UserInterface.printEntreeMenu();
+                DisplayCurrentOrder.displayCurrentOrder();
+                break;
+            case 8:
+                Receipt.printReceipt();
+                UserInterface.additionalOrderOptionsMenu();
+                Input.getAdditionalOrderInput();
+                break;
+        }
+    }
+
+
+
+
+
 }
