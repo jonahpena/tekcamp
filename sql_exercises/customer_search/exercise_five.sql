9c369fd7abd890487e4c605704a129fd4b41b22d
Jonah Pena <jonahrpena@gmail.com>
2022-03-29 23:53:09 -0500

    completed exercise four

--- sql_exercises/customer_search/exercise_five.sql
+++ sql_exercises/customer_search/exercise_five.sql
@@ -1,1 +1,6 @@
-SELECT CONCAT(last_name, ", ",first_name) AS "Full Name" FROM customer WHERE CONCAT(last_name, ", ",first_name) LIKE '%P%';
+SELECT 
+    CONCAT(last_name, ', ', first_name) AS 'Full Name'
+FROM
+    customer
+WHERE
+    CONCAT(last_name, ', ', first_name) LIKE '%P%';
--- sql_exercises/film_counts/exercise_four.sql
+++ sql_exercises/film_counts/exercise_four.sql
@@ -1,2 +1,6 @@
-SELECT store_id, film_id, COUNT(film_id) AS "number of film copies" FROM inventory
-GROUP BY film_id ORDER BY store_id;
+SELECT 
+    store_id, film_id, COUNT(film_id) AS 'number of film copies'
+FROM
+    inventory
+GROUP BY film_id
+ORDER BY store_id;
