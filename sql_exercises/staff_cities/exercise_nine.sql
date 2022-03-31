1e2449815162556ce96bac09c7d6baa99cb66b9c
Jonah Pena <jonahrpena@gmail.com>
2022-03-31 01:26:01 -0500

    completed exercise nine

--- sql_exercises/staff_cities/exercise_nine.sql
+++ sql_exercises/staff_cities/exercise_nine.sql
@@ -1,6 +1,5 @@
 SELECT 
-    staff.first_name AS "First Name", staff.last_name AS "Last Name", 
-    city.city
+    staff.first_name AS "First Name", staff.last_name AS "Last Name"
 FROM
     (staff
 INNER JOIN city ON staff.staff_id = city.city_id)
